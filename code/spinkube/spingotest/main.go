55a115b6e481e0fc308c6d24806a5b6152e51173
David Maat <r1036789@student.thomasmore.be>
2026-03-17 20:47:42 +0100

    testing postgres

--- code/spinkube/spingotest/main.go
+++ code/spinkube/spingotest/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 
 	spinhttp "github.com/spinframework/spin-go-sdk/v2/http"
+	"github.com/spinframework/spin-go-sdk/v2/pg"
 )
+
+func requestValues(r *http.Request) url.Values {
+	values := url.Values{}
+	if r.URL != nil {
+		return r.URL.Query()
+	}
+	if r.RequestURI == "" {
+		return values
+	}
+	parsedURI, err := url.ParseRequestURI(r.RequestURI)
+	if err == nil {
+		return parsedURI.Query()
+	}
+	return values
+}
 
 func init() {
 	mux := http.NewServeMux()
@@ -30,15 +48,7 @@ func init() {
 			return
 		}
 
-		values := url.Values{}
-		if r.URL != nil {
-			values = r.URL.Query()
-		} else if r.RequestURI != "" {
-			parsedURI, err := url.ParseRequestURI(r.RequestURI)
-			if err == nil {
-				values = parsedURI.Query()
-			}
-		}
+		values := requestValues(r)
 
 		lat := values.Get("lat")
 		lon := values.Get("lon")
@@ -71,6 +81,46 @@ func init() {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(resp.StatusCode)
 		_, _ = w.Write(body)
+	})
+
+	mux.HandleFunc("/db-test", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		values := requestValues(r)
+		dsn := values.Get("dsn")
+		if dsn == "" {
+			dsn = os.Getenv("PG_DSN")
+		}
+
+		if dsn == "" {
+			http.Error(w, "missing postgres dsn: set PG_DSN env var or pass ?dsn=...", http.StatusBadRequest)
+			return
+		}
+
+		db := pg.Open(dsn)
+		defer db.Close()
+
+		var one int
+		err := db.QueryRow("SELECT 1").Scan(&one)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadGateway)
+			_ = json.NewEncoder(w).Encode(map[string]any{
+				"ok":    false,
+				"error": err.Error(),
+			})
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(map[string]any{
+			"ok":      true,
+			"result":  one,
+			"message": "postgres connection successful",
+		})
 	})
 
 	spinhttp.Handle(mux.ServeHTTP)
